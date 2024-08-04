07bd0ccd08961ab3c8af4db9434d57c11a4a4f74
Leandro Thiago Ribeiro <111009157+LeandroTRibeiro@users.noreply.github.com>
2024-08-04 19:47:11 -0300

    Initial commit

---
+++ src/list1ex1/list1ex1.java
@@ -0,0 +1,23 @@
+package list1ex1;
+import javax.swing.JOptionPane;
+
+//1. Faça um algoritmo em Java que receba três notas e seus respectivos
+// pesos, calcule e mostre a média ponderada dessas notas.
+
+public class list1ex1 {
+	
+    public static void main(String[] args) {
+    	Integer n1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a primeira nota: "));
+    	Integer p1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o peso da primeira nota: "));
+    	Integer n2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a segunda nota: "));
+    	Integer p2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o peso da segunda nota: "));
+    	Integer n3 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a terceira nota: "));
+    	Integer p3 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o peso da terceira nota: "));
+    	
+    	double med = (n1 * p1 + n2 * p2 + n3 * p3) / (double)(p1 + p2 + p3);
+    	
+    	JOptionPane.showConfirmDialog(null, "A media ponderada das notas e de: " +med);
+    	
+    }
+    
+}
